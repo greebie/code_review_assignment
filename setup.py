7243012363b236f897192332d0966cc732c44f4c
Ryan Deschamps <ryan.deschamps@gmail.com>
2021-10-25 22:12:28 -0400

    add setup.puy

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+   name='Code Review Assignment',
+   version='1.0',
+   description='A useful module',
+   author='Ryan Deschamps',
+   author_email='foomail@foo.com',
+   packages=['code_review', 'tests'],  #same as name
+)
